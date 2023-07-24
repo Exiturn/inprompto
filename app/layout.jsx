ae37f99f7c58e41ab28098ea0d260f09173f956e
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-07-24 17:47:33 +0100

    Major Updates
    -Created an API endpoint for our Google authentication in [...nextauth] folder inside of the API folder. This endpoint allows us to use OAuth from our google API services.
    -Finished Nav component,  still need to add full functionality with authentication providers to show UI for logged in and non-logged in users.
    -Session provider component added.
    -User schema added in models folder.
    -database.js added, using mongoose to connect to our Mongodb database. Database URI is stored as an environment variable.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,46 +1,49 @@
-import '@styles/globals.css'
-import localfont from '@next/font/local'
-import Nav from '@components/Nav'
-import Provider from '@components/Provider'
+import "@styles/globals.css";
+import localfont from "@next/font/local";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-  title: 'Prompto',
-  description: 'Prompto is a simple, modern, and AI powered prompter.',
-}
+  title: "Prompto",
+  description:
+    "Prompto is a simple, modern, and AI powered prompting tool. Share prompts, discover and create new prompts.",
+};
 
-const montreal = localfont ({
+const montreal = localfont({
   src: [
     {
-      path: '../public/fonts/PPNeueMontreal-Medium.otf',
-      weight: '500'
+      path: "../public/fonts/PPNeueMontreal-Medium.otf",
+      weight: "500",
     },
     {
-      path: '../public/fonts/PPNeueMontreal-Bold.otf',
-      weight: '700'
+      path: "../public/fonts/PPNeueMontreal-Bold.otf",
+      weight: "700",
     },
     {
-      path: '../public/fonts/PPNeueMontreal-Book.otf',
-      weight: '400'
-    }
+      path: "../public/fonts/PPNeueMontreal-Book.otf",
+      weight: "400",
+    },
   ],
-  variable: '--font-montreal'
-})
+  variable: "--font-montreal",
+});
 
 const RootLayout = ({ children }) => {
   return (
-   <html lang='en' className={`${montreal.variable} font-montreal`}>
-    <body>
-      <div className='main'>
-        <div className='gradient' />
-      </div>
+    <html lang="en" className={`${montreal.variable} font-montreal`}>
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-      <main className='app'>
-        <Nav />
-        {children}
-      </main>
-    </body>
-   </html>
-  )
-}
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
