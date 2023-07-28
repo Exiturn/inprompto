742e78320f55786150ae160cf0e6de8c550ac17b
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-07-28 02:02:03 +0100

    Create prompt page
    -Created the create prompt page.
    -Finished off the Form component, used inside of the create prompt page.
    -Created an API endpoint for creating prompts, yet to finish.

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import Link from "next/link";
 
-const Form = () => {
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
-    <div>Form</div>
-  )
-}
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type} Post</span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        {type} prompts to share with other users and let AI open up your
+        imagination!
+      </p>
+      <form
+        onSubmit={handleSubmit}
+        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+      >
+        <label>
+          <span className="font-medium text-base text-gray-700">
+            Your AI Prompt
+          </span>
+          <textarea
+            value={post.prompt}
+            onChange={(e) =>
+              setPost({
+                ...post,
+                prompt: e.target.value,
+              })
+            }
+            placeholder="Write your prompt here..."
+            required
+            className="form_textarea"
+          />
+        </label>
 
-export default Form
+        <label>
+          <span className="font-medium text-base text-gray-700">
+            Tags {` `}
+            <span className="font-normal">(#product, #webdev, #ideas)</span>
+          </span>
+          <input
+            value={post.tag}
+            onChange={(e) =>
+              setPost({
+                ...post,
+                tag: e.target.value,
+              })
+            }
+            placeholder="#Tag"
+            required
+            className="form_input"
+          />
+        </label>
+
+        <div className="flex-end mx-3 mb-5 gap-4">
+            <Link href="/" className="text-gray-500">
+              Cancel
+            </Link>
+            <button type="submit" disabled={submitting} className="form_submit">
+              {submitting ? `${type}...}` : type}
+            </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default Form;
