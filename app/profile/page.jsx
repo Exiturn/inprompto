f7d255fc3aae4978195b33048693ab7e17e4cbc3
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-08-04 15:08:14 +0100

    Delete Functionality Finished
    -Added delete functionality.
    -TODO:
    1. Implement search functionality
    2.Tag onClick
    3.View other profiles

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -26,7 +26,21 @@ const MyProfile = () => {
     router.push(`/update-prompt?id=${prompt._id}`)
   };
 
-  const handleDelete = async (prompt) => {};
+  const handleDelete = async (prompt) => {
+    const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${prompt._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPrompts = posts.filter((post) => post._id !== prompt._id);
+        setPosts(filteredPrompts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <Profile
