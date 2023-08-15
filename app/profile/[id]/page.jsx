33ee0df036b5447453d407fd61dcfb13f396e7ba
Bryan Magistrado <bmagistrado@yahoo.com>
2023-08-15 21:45:40 +0100

    View Other Profile Finished
    -Viewing other profile functionality completed
    -Used a new route in the /api/profile/[id] to get the specific profile that was clicked on. Displayed using the same profile component.

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -9,22 +9,36 @@ const otherProfile = ({ params }) => {
   const { data: session } = useSession();
   const [posts, setPosts] = useState("");
 
-
   useEffect(() => {
     const fetchPrompts = async () => {
       const response = await fetch(`/api/users/${params.id}/posts`);
       const data = await response.json();
       setPosts(data);
     };
-    if (session?.user.id) {
+
+    if (session?.user.id !== params.id) {
       fetchPrompts();
     }
-  }, [session?.user.id]);
+  }, []);
 
   useEffect(() => {
     console.log(posts);
-  }, [posts])
-  return <div>{params.id}</div>;
+  }, [posts]);
+
+  return (
+    <div>
+      {posts ? (
+        <Profile
+        name={`${posts[0].creator.username}`}
+        desc={`This is ${posts[0].creator.username}'s profile.`}
+        data={posts}
+        userId={posts[0].creator._id}
+      />
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  );
 };
 
 export default otherProfile;
--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,34 +1,46 @@
+"use client"
+
 import PromptCard from "./PromptCard";
+import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 
-const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
+const Profile = ({ name, desc, data, handleEdit, handleDelete, userId }) => {
+  const { data: session } = useSession();
+
+  useEffect(() => {
+   console.log(session)
+  }, []);
 
   return (
     <section className="w-full">
       <h1 className="head_text text-left">
-        <span className="blue_gradient">{name} Profile</span>
+        <span className="blue_gradient capitalize">{name}'s Profile</span>
       </h1>
-
       <p className="desc text-left">{desc}</p>
-
-      {data ? 
-      <div className="mt-16 tag_header">
-        You have no prompts yet. Start creating new prompts to see them in your profile!
-      </div>
-      :
-      ""}
-
+      {!data ? (
+        <div className="mt-16 tag_header">
+          {session?.user._id === userId
+            ? "You have no prompts yet. Start creating new prompts to see them in your profile!"
+            : "They have not created any prompts yet."}
+        </div>
+      ) : (
+        ""
+      )}
+      
+      {session?.user.id ? <div className="mt-16 tag_header"></div> : ""}
       <div className="mt-10 prompt_layout">
-        {data ? data.map((prompt) => (
-          <PromptCard
-            key={prompt._id}
-            prompt={prompt}
-            handleEdit={() => handleEdit && handleEdit(prompt)}
-            handleDelete={() => handleDelete && handleDelete(prompt)}
-          />
-        )) : 
+        {data ? (
+          data.map((prompt) => (
+            <PromptCard
+              key={prompt._id}
+              prompt={prompt}
+              handleEdit={() => handleEdit && handleEdit(prompt)}
+              handleDelete={() => handleDelete && handleDelete(prompt)}
+            />
+          ))
+        ) : (
           <div>Loading...</div>
-        }
+        )}
       </div>
     </section>
   );
